f4f540a8f329033d5d13e7d754764e7af7a895be
maiersk <poilyoungman@foxmail.com>
2021-02-26 23:34:07 +0800

    add: marked and highlight.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import Router from './plugins/router'
 import FontAwesomeIcon from './plugins/fontawesome'
 import store from './store'
+import hljs from 'highlight.js'
 
 createApp(App)
     .use(store)
     .use(Router)
+    .use(hljs.vuePlugin)
     .component('faIcon', FontAwesomeIcon)
     .mount('#app')
