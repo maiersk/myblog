2edb6351ad18dd4e8922bd0d5468dbe97b79d0a0
maiersk <poilyoungman@foxmail.com>
2021-02-26 23:34:38 +0800

    update: blog item html and css

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -7,31 +7,41 @@
     count: 5,
   }">
     <template #c_item="d">
-      <div class="container blog_div p-1">
-        <div class="title m-1 flex justify-between align-center">
-          <span>{{d.item.title}}</span>
-
-          <div class="tags m-s1">
-            <ul class="c_ul flex align-center">
-              <li v-for="tag in d.item.tags" :key="tag">
-                <tag-btn :name="tag.name" :col="tag.color"></tag-btn>
-              </li>
-            </ul>
+      <div class="container blog_div p-2">
+        <div class="top">
+          <div class="title_item">
+            <span class="title">
+              {{d.item.title}}
+            </span>
+            <span class="time">
+              {{time(d.item.createdAt)}}
+            </span>
           </div>
         </div>
-        <div class="content m-s1">
+        <div class="content">
           <p>
             {{d.item.content}}
           </p>
         </div>
-        <div class="operator flex justify-between align-center">
-          <div class="comments">
-            <span>comments</span>
+        <div class="other_item flex align-center">
+          <div class="comments flex align-center border">
+            <faIcon :icon="['fas', 'comments']"></faIcon>
+            <span class="m-1">{{d.item.comments?.length ?? 0}}</span>
           </div>
-          <div class="time">
-            <span>
-              {{time(d.item.createdAt)}}
-            </span>
+          <div class="viewcount flex align-center">
+            <faIcon :icon="['fas', 'eye']"></faIcon>
+            <span class="m-1">{{d.item?.viewCount ?? 0}}</span>
+          </div>
+
+          <span class="m-1">|</span>
+
+          <div class="tags flex align-center">
+            <faIcon :icon="['fas', 'tags']"></faIcon>
+            <ul class="c_ul m-1 flex">
+              <li v-for="tag in d.item.tags" :key="tag">
+                <tag-btn :name="tag.name" :col="tag.color"></tag-btn>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -157,19 +167,54 @@ export default {
 .blog_div {
   background-color: #282828;
   border-bottom: 1px solid #303030;
+  transition: all 0.5s ease;
 
-  .title {
+  .top {
+    display: table;
+    white-space: nowrap;
+    text-align: center;
 
-    > span {
-      font-size: 24px;
-    } 
+    .title_item {
+      margin: 0 0.5rem;
 
-    .tags {
-
+      .title {
+        font-weight: 600;
+        font-size: 1.4rem;
+        cursor: pointer;
+      }
+      .time {
+        font-size: .5em;
+        margin-left: 20px;
+      }
     }
   }
-  .content {
 
+  .top::before {
+    position: relative;
+    display: table-cell;
+    border-top: 1px solid #e8e8e8;
+    top: 50%;
+    width: 5%;
+    transform: translateY(50%);
+    content: "";
   }
+  .top::after {
+    @extend .top::before;
+    width: 95%;
+  }
+
+  .content {
+    max-height: 260px;
+    margin: 1rem 0;
+    padding: 0.5rem;
+  }
+
+  .other_item {
+    height: 20px;
+  }
+}
+.blog_div:hover {   
+  background-color: rgba(255, 178, 69, 0.5);
+  box-shadow: 0 1px 10px 2px rgba(0, 0, 0, 0);
 }
 </style>
