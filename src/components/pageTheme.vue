cba4a4486ba8f71e4ac1dd097e47e39ce1c1d20c
maiersk <poilyoungman@foxmail.com>
2021-03-01 23:18:43 +0800

    update: blog

--- src/components/pageTheme.vue
+++ src/components/pageTheme.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="page-theme">
-    <div class="container title_div">
-      <div class="path">
-        <faIcon class="faicon" :icon="icon"></faIcon>
-      </div> 
-      <slot name="title">Page Title</slot>
+    <div class="container title_div c_div">
+      <slot name="c_title">
+        <div class="path">
+          <faIcon class="faicon" :icon="icon"></faIcon>
+        </div>
+        <slot name="title">Page Title</slot>  
+      </slot>
     </div>
     <main class="container">
       <slot name="body">Page Body</slot>
@@ -36,13 +38,8 @@ export default {
     flex-grow: 1; //使用剩余高度
 
     .title_div {
-      background-color: #282828;
-      border: 1px solid #303030;
-      display: flex;  
+      display: flex;
       align-items: center;
-      box-shadow: 0 1px 3px rgba(0,0,0,.06), 0 1px 2px rgba(0,0,0,.12);
-      border-radius: 2px;
-      padding: 0.5rem;
     }
     
     .path {
--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,11 +1,51 @@
 <template>
-  <page-theme>
-    <template #title>
-      <span>{{$root.format_time(this.post.createdAt, "YYYY-MM-dd HH:mm")}}</span>
-      <h1>{{this.post.title}}</h1>
+  <page-theme v-if="Object.keys(post).length">
+    <template #c_title>
+      <div class="blog_t w-100 flex-column justify-center align-center">
+        <h1>{{post.title}}</h1>
+
+        <div class="other_item flex align-center">
+          <div class="time">
+            <faIcon class="faicon" :icon="icon"></faIcon>
+            <span>
+              Posted on {{$root.format_time(post.createdAt, "YYYY-MM-dd")}}
+            </span>
+          </div>
+          <span>|</span>
+          <div class="tags flex align-center">
+            <faIcon :icon="['fas', 'tags']"></faIcon>
+            <ul class="c_ul m-1 flex">
+              <li v-for="tag in post.tags" :key="tag">
+                <tag-btn :name="tag.name" :col="tag.color"></tag-btn>
+              </li>
+            </ul>
+          </div>
+          <span>|</span>
+          <div class="comments flex align-center">
+            <faIcon :icon="['fas', 'comments']"></faIcon>
+            <span class="m-1">{{post.comments?.length ?? 0}}</span>
+          </div>
+          <div class="viewcount flex align-center">
+            <faIcon :icon="['fas', 'eye']"></faIcon>
+            <span class="m-1">{{post.viewCount ?? 0}}</span>
+          </div>
+        </div>
+      </div>
     </template>
     <template #body>
-      <div v-html="$root.markedContent(this.post?.content ?? '')"></div>
+      <div class="content" v-html="$root.markedContent(post.content)"></div>
+      <div class="discuss">
+        <div class="discuss_header">
+          <span>{{post.comments.length}}</span>条评论
+          <div class="c-divider c-divider-horizontal"></div>
+        </div>
+        <div class="send_comment">
+
+        </div>
+        <div class="comments">
+          <div class="comment-inner"></div>
+        </div>
+      </div>
     </template>
   </page-theme>
 </template>
@@ -13,17 +53,20 @@
 <script>
 import pageTheme from "../components/pageTheme";
 import { axiosReq } from "../plugins/axios";
+import tagBtn from "../components/Buttons/tagBtn";
 
 export default {
   components: {
     pageTheme,
+    tagBtn
   },
   data() {
     return {
-      post: {}
+      post: {},
+      icon: this.$router.currentRoute.value.meta.icon
     }
   },
-  mounted() {
+  beforeCreate() {
     axiosReq({
       methods: 'get',
       url: `/posts/${this.$route.query?.id ?? 0}?view=1`
@@ -38,6 +81,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .blog_t {
+    > h1 {
+      margin-bottom: 1rem;
+    }
 
+    .other_item {
+      justify-content: space-around;
+
+      > * {
+        padding: 0 8px;
+      }
+    }
+  }
+
+  .content {
+    margin: 0 10px;
+  }
 </style>
