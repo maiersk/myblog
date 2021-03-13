28557df7661a8de500bcd5404175889817e8d50e
maiersk <poilyoungman@foxmail.com>
2021-03-13 23:10:31 +0800

    update: comment reply func

--- src/components/listComments.vue
+++ src/components/listComments.vue
@@ -4,6 +4,7 @@
       <span>{{crudRef?.list?.data?.length ?? 0}}</span>条评论
       <div class="c-divider c-divider-horizontal"></div>
     </div>
+
     <divCRUD name="Comment" url="/comments" 
       ref="crudRef"
       :options="{
@@ -14,6 +15,7 @@
         pagecount: 4,
         count: 5,
       }">
+
       <template #c_operate>
         <commentModel>
           <template #c_content>
@@ -29,11 +31,14 @@
         </commentModel>
       </template>
 
+      <!-- 评论列 -->
       <template #c_item="comment">
         <commentModel class="ptb-1" :model="comment.item">
           <template #c_actions>
             <a class="c_a mr-1"
-              @click.prevent="openReply = !openReply"
+              @click.prevent="openReply === `reply_${comment.item.id}` ?
+                openReply = '' : openReply = `reply_${comment.item.id}`
+              "
             >
               reply
             </a>
@@ -46,22 +51,74 @@
             </a>
           </template>
 
+          <!-- 外部回复组件 -->
           <template #c_rear>
-            <div v-if="openReply">
+            <div v-if="openReply === `reply_${comment.item.id}`">
               <commentModel class="reply" :showAvatar="false">
                 <template #c_content>
                   <textarea class="textarea_c" rows="4"
-                    v-model="reply.content"
+                    :ref="`rpy_c_${comment.item.id}`"
                   ></textarea>
 
                   <base-btn class="ml-auto"
                     btnValue="Send"
-                    @click.prevent="reqReply(comment.item.id)"
+                    @click.prevent="add_Reply(comment.item.id,
+                      $refs[`rpy_c_${comment.item.id}`].value
+                    )"
                   ></base-btn>
                 </template>
               </commentModel>
             </div>
+
+            <!-- 回复评论列 -->
+            <div v-if="comment.item?.replies?.length !== 0">
+              <div class="reply_comment"
+                v-for="(reply, i) in comment.item.replies"
+                :key="i"
+              >
+                <commentModel class="ptb-1"
+                  :model="reply">
+                  <template #c_actions>
+                    <a class="c_a mr-1"
+                      @click.prevent="openReply === `reply_${reply.id}_inner` ?
+                        openReply = '' : openReply = `reply_${reply.id}_inner`
+                      "
+                    >
+                      reply
+                    </a>
+
+                    <a class="c_a delete_btn text-c-red"
+                      v-if="state.isLogged"
+                      @click.prevent="del_Reply(reply.id)"
+                    >
+                      <faIcon :icon="['fas', 'trash-alt']"></faIcon>
+                    </a>
+                  </template>
+
+                  <!-- 内部回复组件 -->
+                  <template #c_rear>
+                    <div v-if="openReply === `reply_${reply.id}_inner`">
+                      <commentModel class="reply" :showAvatar="false">
+                        <template #c_content>
+                          <textarea class="textarea_c" rows="4"
+                            :ref="`rpy_c_${i}_inner`"
+                          ></textarea>
+
+                          <base-btn class="ml-auto"
+                            btnValue="Send"
+                            @click.prevent="add_Reply(comment.item.id,
+                              $refs[`rpy_c_${i}_inner`].value
+                            )"
+                          ></base-btn>
+                        </template>
+                      </commentModel>
+                    </div>
+                  </template>
+                </commentModel>
+              </div> 
+            </div>
           </template>
+
         </commentModel>
       </template>
     </divCRUD>
@@ -116,31 +173,42 @@ export default {
         userId: null,
         replyId: null
       },
-      reply: {
-        content: null,
-      },
       state: this.$store.state,
       comments: [],
       crudRef: null,
-      openReply: false,
+      openReply: '',
     }
   },
   mounted() {
     this.crudRef = this.$refs['crudRef']
   },
   methods: {
-    reqReply(commentId) {
+    add_Reply(commentId, content) {
       axiosReq({
         method: 'post',
         url: '/comments/reply',
         data: {
           commentId,
-          content: this.reply.content
+          content
         }
       }).then((res) => {
         console.log(res)
 
-        this.$root.openNotifi(true, 'test')
+        this.crudRef.getAll()
+        this.$root.openNotifi(true, 'Send success')
+      }).catch((err) => {
+        this.$root.openNotifi(false, err)
+      })
+    },
+    del_Reply(replyId) {
+      axiosReq({
+        method: 'delete',
+        url: `/comments/reply/${replyId}`,
+      }).then((res) => {
+        console.log(res)
+
+        this.crudRef.getAll()
+        this.$root.openNotifi(true, 'delet success')
       }).catch((err) => {
         this.$root.openNotifi(false, err)
       })
@@ -163,6 +231,10 @@ export default {
 
     .reply {
       padding: 1rem 0 0 1rem;
+    }
+
+    .reply_comment {
+      padding: 1rem 0 0 3.5rem;
     }
   }
 </style>
