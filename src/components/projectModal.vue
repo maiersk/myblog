bc899be48f24244e6c3f8c73bf149dbe39f4cfb0
maiersk <poilyoungman@foxmail.com>
2021-03-26 00:08:04 +0800

    update: sync

---
+++ src/components/projectModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container project_div">
+    <div class="image">
+      <img src="" alt="">
+    </div>
+    <div class="connect">
+      <div class="project_name">
+        
+      </div>
+      <div class="desction">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
